7f9767b225c4554e3da92ed97c9258754a3fd103
khushboo545567 <khushbooyadav6266763530@gmail.com>
2026-01-05 22:31:05 +0530

    .

--- backend/src/worker/result.worker.js
+++ backend/src/worker/result.worker.js
@@ -1,12 +1,14 @@
 import resultQueue from "../queue/result.queue";
 import SubmittedAnswer from "../models/submittedans.model.js";
 import { Question } from "../models/question.model.js";
+import sendResultMail from "../utils/mail.js";
+import { User } from "../models/user.model.js";
 
 resultQueue.process(async (job) => {
   const { userId, testId } = job.data;
 
   console.log("Processing result for:", userId, testId);
-
+  const user = await User.findById(userId);
   const submission = await SubmittedAnswer.findOne({ userId, testId });
 
   const questions = await Question.find({ testId });
@@ -28,4 +30,12 @@ resultQueue.process(async (job) => {
   await submission.save();
 
   //   call send mail function
+
+  await sendResultMail(
+    user.email,
+    "Your Test Result",
+    `<h3>Hello ${user.userName}</h3>
+     <p>Your test is evaluated.</p>
+     <p><b>Score:</b> ${totalScore}</p>`
+  );
 });
