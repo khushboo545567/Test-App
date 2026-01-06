47ae815a6be41f6573c41d94fb3d1332178be7d5
khushboo545567 <khushbooyadav6266763530@gmail.com>
2026-01-06 18:37:54 +0530

    fixed result queue

--- backend/src/worker/result.worker.js
+++ backend/src/worker/result.worker.js
@@ -5,37 +5,50 @@ import sendResultMail from "../utils/mail.js";
 import { User } from "../models/user.model.js";
 
 resultQueue.process(async (job) => {
-  const { userId, testId } = job.data;
+  try {
+    const { userId, testId, submissionId } = job.data;
 
-  console.log("Processing result for:", userId, testId);
-  const user = await User.findById(userId);
-  const submission = await SubmittedAnswer.findOne({ userId, testId });
+    console.log("Processing result for:", userId, testId);
+    const user = await User.findById(userId);
+    // const submission = await SubmittedAnswer.findOne({ userId, testId });
+    const submission = await SubmittedAnswer.findById(submissionId);
+    if (!submission) throw new Error("Submission not found");
 
-  const questions = await Question.find({ testId });
+    const questions = await Question.find({ testId });
 
-  let totalScore = 0;
+    let totalScore = 0;
 
-  submission.answers.forEach((ans) => {
-    const question = questions.find(
-      (q) => q._id.toString() === ans.questionId.toString()
-    );
+    submission.answers.forEach((ans) => {
+      const question = questions.find(
+        (q) => q._id.toString() === ans.questionId.toString()
+      );
 
-    if (question && question.answer === ans.answer) {
-      ans.score = question.mark || 1;
-      totalScore += ans.score;
-    }
-  });
+      if (question && question.answer === ans.answer) {
+        ans.score = question.mark || 1;
+        totalScore += ans.score;
+      }
+    });
 
-  submission.totalScore = totalScore;
-  await submission.save();
+    submission.totalScore = totalScore;
+    await submission.save();
 
-  //   call send mail function
+    //   call send mail function
 
-  await sendResultMail(
-    user.email,
-    "Your Test Result",
-    `<h3>Hello ${user.userName}</h3>
+    await sendResultMail(
+      user.email,
+      "Your Test Result",
+      `<h3>Hello ${user.userName}</h3>
      <p>Your test is evaluated.</p>
      <p><b>Score:</b> ${totalScore}</p>`
-  );
+    );
+  } catch (error) {
+    console.error("Result job failed:", error);
+    throw error;
+  }
 });
+
+await resultQueue.add(
+  "calculate-result",
+  { userId, testId, submissionId: submission._id },
+  { attempts: 3, backoff: 5000 }
+);
